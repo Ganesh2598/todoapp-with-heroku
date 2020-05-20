0de0727bba4066df369b4971d41698267f88313f
ganesh2598 <kratosganesh2598.com>
2020-05-20 17:53:20 +0530

    changed

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const path = require("path");
 if (process.env.NODE_ENV === 'production') { 
     app.use(express.static("build"));
     app.get("*",(req,res)=>{
-        res.sendFile(path.join(__dirname,"Client","build","index.html"));
+        res.sendFile(path.join(__dirname,"Client/build/index.html"));
     })
  }
 const port = process.env.PORT || 8000;
