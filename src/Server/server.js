794787baafde871fdc466f292b339833a3d590ea
ganesh2598 <kratosganesh2598.com>
2020-05-20 09:50:25 +0530

    init

--- src/Server/server.js
+++ src/Server/server.js
@@ -3,17 +3,17 @@ const cors = require("cors");
 const pool = require("pg").Pool;
 const app = express();
 const path = require("path");
-const port = process.env.port || 8000;
+const port = process.env.PORT || 8000;
 
 app.use(cors());
 app.use(express.json());
 
 const connect = new pool({
-    user : "postgres",
-    password : "kratos",
-    host : "localhost",
+    user : "oxbfeyidmpgzcp",
+    password : "816e0441af0e4be6b405d518ec53e0f1ead72955f24c7e44795c091cf5e71c02",
+    host : "ec2-34-195-169-25.compute-1.amazonaws.com",
     port : 5432,
-    database : "heroku-postgresql"
+    database : "d5g5g39cittq6e"
 });
 
 
