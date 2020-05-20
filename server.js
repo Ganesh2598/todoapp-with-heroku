1ed564af0b6b695465dd1833323cbfe937618db8
ganesh2598 <kratosganesh2598.com>
2020-05-20 23:15:12 +0530

    added history

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const pool = require("pg").Pool;
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.urlencoded());
 
 const path = require("path");
 if (process.env.NODE_ENV === 'production') { 
