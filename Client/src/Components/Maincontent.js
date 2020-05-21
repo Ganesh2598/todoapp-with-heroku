474523f2c6fa2eb354eb717884a3c01c4be0dbe0
ganesh2598 <kratosganesh2598.com>
2020-05-21 10:49:26 +0530

    modified

--- Client/src/Components/Maincontent.js
+++ Client/src/Components/Maincontent.js
@@ -32,7 +32,8 @@ class Maincontent extends Component{
 
     async componentDidMount(){
         try {
-            const response = await fetch(`/todos/`,{
+            const response = await fetch(`/todos/${this.user_id}`,{
+                method : "GET",
                 headers:{"Content-Type":"application/json","Accept":"application/json"}
             });
             const data = await response.json();
