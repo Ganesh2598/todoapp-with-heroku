cfa4af3095c1384a48a7012c43375aac46fa08a0
ganesh2598 <kratosganesh2598.com>
2020-05-26 12:45:31 +0530

    undo

--- Client/src/Components/Maincontent.js
+++ Client/src/Components/Maincontent.js
@@ -95,7 +95,7 @@ class Maincontent extends Component{
         }
         //console.log(this.state.list)
         const updated = this.state.list.filter(task => task !== item.item)
-        const deleteitem = [this.state.deleted,item.item]
+        const deleteitem = [...this.state.deleted,item.item]
         this.setState({
             list:updated,
             deleted:deleteitem
@@ -108,6 +108,10 @@ class Maincontent extends Component{
         try{
             if(this.state.deleted.length>0){
                 let task = this.state.deleted[this.state.deleted.length-1]
+                const updateDelete = this.state.deleted.filter(task => task !== task)
+                this.setState({
+                    deleted : updateDelete
+                })
                 let id = this.user_id
                 const body = {task,id}
                 const response = await fetch("/todos/",{
